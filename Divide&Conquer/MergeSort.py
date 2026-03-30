84bcc735395cc9bc081139d62b85c0317aadcb39
cjlr976 <chloejlrobinson@gmail.com>
2026-03-30 11:43:19 -0400

    Cleanup Test Case

--- Divide&Conquer/MergeSort.py
+++ Divide&Conquer/MergeSort.py
@@ -1,23 +1,25 @@
-# Authors: Chloe Robinson, Camila Fienco
+# Authors: Chloe Robinson, Camila Fienco, Tiffany Hsu
 # Purpose: Implementing Merge Sort using Divide and Conquer
 
 # Function: Merge Sort
-# Input: An unsorted array
+# Input: An array
 # Output: A sorted array
+from math import floor
+
 def merge_sort(arr):
     #Base case
     if len(arr) <= 1:
         return arr
 
     #Divide recursively
-    mid = len(arr) // 2
+    mid = floor(len(arr) / 2)
     left_half = merge_sort(arr[:mid])
     right_half = merge_sort(arr[mid:])
 
-    #Conquer
+    #Combine
     return merge(left_half, right_half)
 
-#Combine
+#Conquer
 def merge(left, right):
     merged = []
     i = j = 0
@@ -41,7 +43,7 @@ def main():
     unsorted_array = [5, 42, 14, 20, 1, 24, 76, 58]
     reverseSorted_array = [123, 86, 54, 32, 20, 12, 9]
 
-    choice = input("Enter 1 for sorted array, 2 for unsorted array, 3 for reverse sorted array: ")
+    choice = input("1:Sorted array\n2:Unsorted array\n3:Reverse sorted array:\nEnter: ")
     if choice == '1':
         result = merge_sort(sorted_array)
     elif choice == '2':
